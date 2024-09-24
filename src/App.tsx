ac40e42f05e9893a94c58c9a1f33a75f09198e8c
Pratik Satpathy <81401828+prsatpat1711@users.noreply.github.com>
2024-09-24 20:53:40 +0530

    Basic site developed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 
 function App() {
-
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        </Routes>
-    </BrowserRouter>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
